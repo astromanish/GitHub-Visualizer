55e241e0db37427d66ac8b7dfc54faeac2e1afec
Manish Singh <astromanishsingh@gmail.com>
2024-03-13 13:01:54 +0530

    search page updated

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,31 +1,46 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Box, Container, Grid, TextField, Button, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Search() {
-    const [name, setUsername] = useState({
-        userName: ''
-    });
+    const [userName, setUserName] = useState('');
 
     const changeUsername = (e) => {
-        setUsername({userName: e.target.value})
+        setUserName(e.target.value);
     }
 
     return (
-        <>
-            <div className="cntnr">
-                <h2 className="main-heading display-4"><img height="52" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/github.svg" /></h2>
-                <form>
-                    <div className="form-group">
-                        <input type="text" value={name.userName} onChange={changeUsername} placeholder="Enter Username" className="form-control"/>
-                    </div>
-                    <div  className="text-center">
-                        <Link to={'/' + name.userName} className="text-light"><button className="btn btn-dark"> Lets Go!  </button> </Link>
-                    </div>
-                </form>
-            </div>
-        </>
-
-    )
+        <Box sx={{ width: '100vw', height: '100vh', backgroundColor: '#808080' }}>
+            <Container sx={{ height: '100%' }}>
+                <Grid container justifyContent="center" alignItems="center" height="100%">
+                    <Grid item xs={12} sm={4} sx={{ backgroundColor: 'white', paddingTop: '120px', paddingBottom: '120px', borderRadius: '20px', paddingLeft: '20px', paddingRight: '20px', textAlign: 'center' }}>
+                        <GitHubIcon sx={{ fontSize: 64, color: '#424242' }} />
+                        <Typography variant="h4" align="center" gutterBottom sx={{ fontSize: 64, fontFamily: '"Style Script", cursive', fontWeight: 400, fontStyle: 'normal', color: '#424242' }}>
+                            Activities
+                        </Typography>
+                        <form>
+                            <TextField
+                                fullWidth
+                                value={userName}
+                                onChange={changeUsername}
+                                label="Enter Github Username"
+                                variant="outlined"
+                                margin="normal"
+                            />
+                            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                                <Link to={'/' + userName} style={{ textDecoration: 'none' }}>
+                                    <Button variant="contained" color="primary" size="large" sx={{ backgroundColor: '#424242' }}>
+                                        Let's Go!
+                                    </Button>
+                                </Link>
+                            </div>
+                        </form>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
 }
 
-export default Search
+export default Search;
