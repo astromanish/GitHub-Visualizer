5fc90ae5bd63e201563c89f48a3b9e3b33ea4413
Manish Singh <astromanishsingh@gmail.com>
2024-03-13 14:40:25 +0530

    home page done

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { Box, Container, Grid, TextField, Button, Typography, List, ListItem, ListItemAvatar, Avatar, ListItemText } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
@@ -35,9 +36,9 @@ function Search() {
 
     return (
         <Box sx={{ width: '100vw', height: '100vh', backgroundColor: '#808080' }}>
-            <Container sx={{ height: '100%' }}>
+            <Container sx={{ height: '100%'}}>
                 <Grid container justifyContent="center" alignItems="center" height="100%">
-                    <Grid item xs={12} sm={4} sx={{ backgroundColor: 'white', paddingTop: '120px', paddingBottom: '120px', borderRadius: '20px', paddingLeft: '20px', paddingRight: '20px', textAlign: 'center' }}>
+                    <Grid item xs={12} sm={4} sx={{ backgroundColor: 'white', paddingTop: '120px', paddingBottom: '120px', borderRadius: '20px', paddingLeft: '20px', paddingRight: '20px', textAlign: 'center', marginTop: '20px'}}>
                         <GitHubIcon sx={{ fontSize: 64, color: '#424242' }} />
                         <Typography variant="h4" align="center" gutterBottom sx={{ fontSize: 64, fontFamily: '"Style Script", cursive', fontWeight: 400, fontStyle: 'normal', color: '#424242' }}>
                             activities
@@ -52,7 +53,7 @@ function Search() {
                         />
                         <List>
                             {suggestions.map((user) => (
-                                <ListItem key={user.id} button component="a" href={user.html_url} target="_blank">
+                                <ListItem key={user.id} button component="a" href={user.html_url} target="_blank" sx={{ border: '1.3px solid #808080', borderRadius: '5px', marginBottom: '5px' }}>
                                     <ListItemAvatar>
                                         <Avatar alt={user.login} src={user.avatar_url} />
                                     </ListItemAvatar>
@@ -61,9 +62,11 @@ function Search() {
                             ))}
                         </List>
                         <div style={{ textAlign: 'center', marginTop: '20px' }}>
-                            <Button variant="contained" color="primary" size="large" sx={{ backgroundColor: '#424242', borderRadius: '20px' }}>
-                                Let's Go!
-                            </Button>
+                            <Link to={`/${userName}`} style={{ textDecoration: 'none' }}>
+                                <Button variant="contained" color="primary" size="large" sx={{ backgroundColor: '#424242', borderRadius: '20px' }}>
+                                    Let's Go!
+                                </Button>
+                            </Link>
                         </div>
                     </Grid>
                 </Grid>
